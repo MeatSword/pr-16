8ed0a00df671165503b4cc8d7b129f541aa5166c
Alexander <thealexmaps@yandex.ru>
2023-11-13 08:34:29 +0700

    two

---
+++ app/src/main/java/com/example/pr16_vershinim_ai/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.pr16_vershinim_ai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etText;
+    Button btnSave, btnLoad;
+
+    SharedPreferences sPref;
+
+    final String SAVED_TEXT = "saved_text";
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etText = (EditText) findViewById(R.id.etText);
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+
+        btnLoad = (Button) findViewById(R.id.btnLoad);
+
+    }
+
+
+    public void onClick(View v) {
+
+        if(v.getId() == R.id.btnSave){
+            saveText();
+        }
+        else if(v.getId() == R.id.btnLoad){
+            loadText();
+        }
+    }
+
+    void saveText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putString(SAVED_TEXT, etText.getText().toString());
+        ed.commit();
+        Toast.makeText(this, "Text saved", Toast.LENGTH_SHORT).show();
+    }
+
+    void loadText() {
+        sPref = getPreferences(MODE_PRIVATE);
+        String savedText = sPref.getString(SAVED_TEXT, "");
+        etText.setText(savedText);
+        Toast.makeText(this, "Text loaded", Toast.LENGTH_SHORT).show();
+    }
+}
